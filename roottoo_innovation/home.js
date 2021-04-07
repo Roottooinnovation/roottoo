e257d35d8a67bb891d664b448d015bfb8ef315e8
Roottooinnovation <82092935+Roottooinnovation@users.noreply.github.com>
2021-04-07 16:22:52 +0530

    Add files via upload

---
+++ roottoo_innovation/home.js
@@ -0,0 +1,297 @@
+function myFunctionbar() {
+    var xy = document.getElementById("myLinks");
+    var verlist=document.getElementById("verlist");
+    var link=document.getElementsByClassName("optionlink");
+    if (xy.style.display === "none") {
+      xy.style.display = "block";
+      // verlist.style.display = "none";
+    } else {
+      xy.style.display = "none";
+    }
+}
+
+function myFunctionver() {
+    // var xy = document.getElementById("myLinks");
+    // var ri___text = document.getElementById("ri___text");
+    var verlist=document.getElementById("verlist");
+    var x = document.getElementById("myDIV");
+    var y=document.getElementById("rit");
+    var z=document.getElementById("rfq1");
+    var staffing=document.getElementById("staffing");
+    var auditing=document.getElementById("auditing");
+    var sw_dev=document.getElementById("s/w_dev");
+    var trainingdev=document.getElementById("trainingdev");
+    var verlist=document.getElementById("verlist");
+
+    if (verlist.style.display === "block") {
+        verlist.style.display = "none";
+    //   ri___text.style.display= "block";
+    } else {
+        verlist.style.display = "block";
+        x.style.display = "none";
+      y.style.display = "none";
+      z.style.display = "none";
+      staffing.style.display = "none";
+      auditing.style.display ="none";
+      sw_dev.style.display="none"; 
+      trainingdev.style.display = "none";
+    //   ri___text.style.display= "none";
+    }
+}
+
+
+// let riteam=document.getElementById("riteam");
+
+// let frq=document.getElementById("rfq");
+
+// let rfp=document.getElementById("rfp").addEventListner('click', displayrfp);
+
+
+function myFunctionbar() {
+  var xy = document.getElementById("myLinks");
+  var verlist=document.getElementById("verlist");
+  var ri___text = document.getElementById("ri___text");
+  if (xy.style.display === "block") {
+    xy.style.display = "none";
+    ri___text.style.display= "block";
+    verlist.style.display = "none";
+  } else {
+    xy.style.display = "block";
+    ri___text.style.display= "none";
+  //   verlist.style.display = "none";
+  }
+}
+
+function myFunctionver() {
+  // var xy = document.getElementById("myLinks");
+  // var ri___text = document.getElementById("ri___text");
+  var verlist=document.getElementById("verlist");
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+
+  if (verlist.style.display === "block") {
+      verlist.style.display = "none";
+  //   ri___text.style.display= "block";
+  } else {
+      verlist.style.display = "block";
+      x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
+    staffing.style.display = "none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+  //   ri___text.style.display= "none";
+  }
+}
+
+
+let riteam=document.getElementById("riteam");
+
+let frq=document.getElementById("rfq");
+
+let rfp=document.getElementById("rfp").addEventListner('click', displayrfp);
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+ 
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    staffing.style.display = "none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionrfq() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+    y.style.display = "none";
+    x.style.display = "none";
+    staffing.style.display = "none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    z.style.display = "none";
+ }
+}
+
+function myFunctionrfq() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+
+  if (z.style.display === "none") {
+    z.style.display = "block";
+    y.style.display = "none";
+    x.style.display = "none";
+    staffing.style.display = "none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    z.style.display = "none";
+ }
+}
+
+
+
+function myFunctionri() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+    z.style.display = "none";
+    x.style.display = "none";
+    staffing.style.display = "none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    y.style.display = "none";
+  }
+}
+
+
+function myStaffing() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+  if (staffing.style.display === "none") {
+    staffing.style.display = "block";
+    z.style.display = "none";
+    x.style.display = "none";
+    y.style.display ="none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    staffing.style.display = "none";
+  }
+}
+
+function myauditing() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+  if (auditing.style.display === "none") {
+    staffing.style.display = "none";
+    z.style.display = "none";
+    x.style.display = "none";
+    y.style.display ="none";
+    auditing.style.display ="block";
+    sw_dev.style.display="none"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    auditing.style.display = "none";
+  }
+}
+
+function myswdev() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+  if (sw_dev.style.display === "none") {
+    staffing.style.display = "none";
+    z.style.display = "none";
+    x.style.display = "none";
+    y.style.display ="none";
+    auditing.style.display ="none";
+    sw_dev.style.display="block"; 
+    trainingdev.style.display = "none";
+    verlist.style.display = "none";
+  } 
+  else {
+    sw_dev.style.display = "none";
+  }
+}
+
+function mydev() {
+  var x = document.getElementById("myDIV");
+  var y=document.getElementById("rit");
+  var z=document.getElementById("rfq1");
+  var staffing=document.getElementById("staffing");
+  var auditing=document.getElementById("auditing");
+  var sw_dev=document.getElementById("s/w_dev");
+  var trainingdev=document.getElementById("trainingdev");
+  var verlist=document.getElementById("verlist");
+  if (trainingdev.style.display === "none") {
+    staffing.style.display = "none";
+    z.style.display = "none";
+    x.style.display = "none";
+    y.style.display ="none";
+    auditing.style.display ="none";
+    sw_dev.style.display="none"; 
+    verlist.style.display = "none";
+    trainingdev.style.display = "block"
+  } 
+  else {
+      trainingdev.style.display = "none";
+  }
+}
